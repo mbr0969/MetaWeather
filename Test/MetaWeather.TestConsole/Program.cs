29354f86efa6e38734a61d210eb4754983b658e0
mbr0959 <mbr0969@gmail.com>
2021-05-31 16:05:16 +0300

    Настройка тестовго консольго проекта

--- Test/MetaWeather.TestConsole/Program.cs
+++ Test/MetaWeather.TestConsole/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace MetaWeather.TestConsole
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace MetaWeather.TestConsole {
+    class Program {
+
+        private static IHost _Hosting;
+
+        public static IHost Hosting => _Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;  
+
+        public static IHostBuilder CreateHostBuilder(string[] args) {
+            return Host
+                       .CreateDefaultBuilder(args)
+                       .ConfigureServices(ConfigureServices);
+        }
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services) {
+            
+        }
+
+        static async Task Main(string[] args) {
+            using var host = Hosting;
+            await host.StartAsync();
+
+            Console.WriteLine("Stopping");
+            Console.ReadLine();
+            await host.StopAsync();
+
         }
     }
 }
